e8e24e6ed9a1933b428604de1d353464662c0c92
Nicu Blanaru <nickblanru@gmail.com>
2026-08-28 22:40:25 +0300

    PRGORESS

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function showSection(sectionId) {
     document.getElementById('projects').style.display = 'none';
     document.getElementById('about').style.display = 'none';
     document.getElementById('contact').style.display = 'none';
+    document.getElementById('bug-space-invasion').style.display = 'none';
 
     // 2. Turn on just the one we asked for
     document.getElementById(sectionId).style.display = 'block';
@@ -14,3 +15,24 @@ document.querySelector('a[href="#projects"]').onclick = function() { showSection
 document.querySelector('a[href="#about"]').onclick = function() { showSection('about'); };
 document.querySelector('a[href="#home"]').onclick = function() { showSection('home'); };
 document.querySelector('a[href="#contact"]').onclick = function() { showSection('contact'); };
+document.querySelector('a[href="#bug-space-invasion"]').onclick = function() { showSection('bug-space-invasion'); };
+
+
+
+let currentImgIndex = 0; 
+
+function changeImage(direction) {
+    const images = document.querySelectorAll('.gallery-img');
+    images[currentImgIndex].classList.remove('active-img');
+    currentImgIndex = currentImgIndex + direction;
+    
+    if (currentImgIndex >= images.length) {
+        currentImgIndex = 0;
+    } 
+    else if (currentImgIndex < 0) {
+        currentImgIndex = images.length - 1;
+    }
+    
+    // Turn on the new image
+    images[currentImgIndex].classList.add('active-img');
+}
